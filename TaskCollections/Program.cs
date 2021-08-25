e8c088ededea0436835ce03cf2de4d7bb60ce2f9
Yana <kolobaeva1390@gmail.com>
2021-08-25 15:32:36 +0300

    Add project files.

---
+++ TaskCollections/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TaskCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
